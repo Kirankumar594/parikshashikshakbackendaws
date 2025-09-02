aeb4eb4cd64a33e996c00c6380e60957aeafd042
Kiran Kumar <kirankumar594@example.com>
2025-09-02 12:45:39 +0530

    qwerty

--- Routes/Admin/Weighttageofthecontent.js
+++ Routes/Admin/Weighttageofthecontent.js
@@ -1,3 +1,31 @@
+// const express = require("express");
+// const {
+//   addweightage,
+//   getallcontent,
+//   updateallcontent,
+//   deleteweightage,
+// } = require("../../Controller/Admin/Weighttageofthecontent");
+// const { Authentication, Authorization } = require("../../Authentication/auth");
+// const router = express.Router();
+
+// router.post("/addweightage", Authentication, Authorization, addweightage);
+// router.get("/getallcontent", getallcontent);
+// router.put(
+//   "/updateallcontent",
+//   Authentication,
+//   Authorization,
+//   updateallcontent
+// );
+// router.delete(
+//   "/deleteweightage/:id/:authId",
+//   Authentication,
+//   Authorization,
+//   deleteweightage
+// );
+// module.exports = router; 
+ 
+ 
+ 
 const express = require("express");
 const {
   addweightage,
@@ -9,6 +37,7 @@ const { Authentication, Authorization } = require("../../Authentication/auth");
 const router = express.Router();
 
 router.post("/addweightage", Authentication, Authorization, addweightage);
+// Updated route to support pagination and search
 router.get("/getallcontent", getallcontent);
 router.put(
   "/updateallcontent",
@@ -22,4 +51,5 @@ router.delete(
   Authorization,
   deleteweightage
 );
+
 module.exports = router;
